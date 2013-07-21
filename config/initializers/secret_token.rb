73c67a2c72fc229f2f476fc6d98107db28fab0e9
Amy Lukima <amylukima@gmail.com>
2013-07-21 16:30:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RsvpOnline::Application.config.secret_key_base = '07d2c1f40c4435823466a31f6ff22f9e0b18f56fa6f658dfd2e2a7f45f3ab79654dfb69f45689c00e8fccea29cfeb9713a1cd2697c548795e4dbb27603d707a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RsvpOnline::Application.config.session_store :cookie_store, key: '_rsvp-online_session'
